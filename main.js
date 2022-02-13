3925657693430608917939600575782a7652eb2a
ejffoddl1 <ejffoddl1@naver.com>
2022-02-13 21:46:13 +0900

    (220213) JavaScript 기초

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log('HEROPY')
+
+console.log(new Date('2022-02-13').getDay());
+
+function helloFunc() {
+
+    console.log(1234);
+}
+helloFunc();
+
+function returnFunc() {
+    return 123;
+}
+let a = returnFunc();
+console.log(a);
