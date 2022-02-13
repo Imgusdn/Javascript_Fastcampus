d0a4438509233462574976386f07343ba10a1f4f
ejffoddl1 <ejffoddl1@naver.com>
2022-02-13 21:51:18 +0900

    (220213) function

--- main.js
+++ main.js
@@ -11,5 +11,15 @@ helloFunc();
 function returnFunc() {
     return 123;
 }
-let a = returnFunc();
-console.log(a);
+//let a = returnFunc();
+//console.log(a);
+
+function sum(a, b) {
+    return a+b;
+}
+
+let a = sum(1,2);
+let b = sum(3,6);
+let c = sum(4,8);
+
+console.log(a,b,c);
