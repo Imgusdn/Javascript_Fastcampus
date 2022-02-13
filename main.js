af981d49ebcadfae82006bc280887b16f0fb818a
ejffoddl1 <ejffoddl1@naver.com>
2022-02-14 00:07:40 +0900

    (220214) 메소드 체이닝

--- main.js
+++ main.js
@@ -95,9 +95,22 @@ boxEls.forEach(function (boxEl,index) {
 })
 */
 
+/*
 const boxEl = document.querySelector('.box');
 
 console.log(boxEl.textContent);
 
 boxEl.textContent = "HEROPY";
 console.log(boxEl.textContent);
+*/
+
+/*
+const a = 'Hello~';
+//split : 문자를 인수 기준으로 쪼개서 배열로 반환.
+//reverse : 배열을 뒤집기.
+//join : 배열을 인수 기준으로 문자로 병합해 반환.
+const b = a.split('').reverse().join('');   //메소드 체이닝 : 메소드를 붙여서 연결
+
+console.log(a); // Hello~
+console.log(b); // ~olleH
+*/
