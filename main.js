8d47962dfc450ae3a00040d555f39cd49102f975
ejffoddl1 <ejffoddl1@naver.com>
2022-02-13 22:04:07 +0900

    (220213) method

--- main.js
+++ main.js
@@ -14,12 +14,41 @@ function returnFunc() {
 //let a = returnFunc();
 //console.log(a);
 
-function sum(a, b) {
+function sum(a, b) { //매개변수
     return a+b;
 }
 
-let a = sum(1,2);
+let a = sum(1,2);   //인수
 let b = sum(3,6);
 let c = sum(4,8);
 
 console.log(a,b,c);
+
+//익명함수 -> 데이터로서 활용 및 변수에 할당하여 사용
+let world = function () {
+    console.log('world');
+}
+world();
+
+//객체 데이터
+const heropy = {
+    name: 'HEROPY',
+    age: 85,
+    //메소드
+    getName: function (){
+        return this.name;
+    },
+    getAge: function() {
+        return this.age;
+    }
+}
+
+const hisName = heropy.getName();
+console.log(hisName);
+
+console.log(heropy.getName());
+
+const hisAge = heropy.getAge();
+console.log(hisAge);
+
+console.log(heropy.getAge());
