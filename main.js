a5f20430159cce7edca5eef3c91a6835010f3608
ejffoddl1 <ejffoddl1@naver.com>
2022-02-13 22:55:41 +0900

    (220213)textContent getter,setter

--- main.js
+++ main.js
@@ -86,9 +86,18 @@ isContains = boxEl.classList.contains('active');
 console.log(isContains);
 */
 
+/*
 const boxEls = document.querySelectorAll('.box');
 
 boxEls.forEach(function (boxEl,index) {
     boxEl.classList.add(`order-${index+1}`)
     console.log(index, boxEl);
 })
+*/
+
+const boxEl = document.querySelector('.box');
+
+console.log(boxEl.textContent);
+
+boxEl.textContent = "HEROPY";
+console.log(boxEl.textContent);
